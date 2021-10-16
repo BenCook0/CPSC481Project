b2e75af2904bc8de040ac5d97e3943c49ccb5332
BenCook0 <bencook03@gmail.com>
2021-10-15 19:24:52 -0600

    added initial project setup

---
+++ CPSC481/CPSC481/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CPSC481
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
