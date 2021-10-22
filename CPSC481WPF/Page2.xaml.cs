d76d0e245de687b5711033c0f2e90f2942109c3d
Leslie Nguyen <43118909+Lesliee-n@users.noreply.github.com>
2021-10-21 19:41:55 -0600

    Add files via upload
    initial files

---
+++ CPSC481WPF/Page2.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CPSC481WPF
+{
+    /// <summary>
+    /// Interaction logic for Page2.xaml
+    /// </summary>
+    public partial class Page2 : Page
+    {
+        public Page2()
+        {
+            InitializeComponent();
+        }
+        private void LogOutButtonClick(object sender, RoutedEventArgs e)
+        {
+            this.NavigationService.Navigate(new Page1());
+            this.NavigationService.RemoveBackEntry();
+        }
+
+        //goals functions below
+        private void setGoalButtonClick(object sender, RoutedEventArgs e)
+        {
+            setGoalPopup.IsOpen = true;
+        }
+    }
+}
