f39900186d7d96da11bc58260f107739d0dfe76a
BenCook0 <bencook03@gmail.com>
2021-10-25 15:35:58 -0600

    potentially added popup stuff

---
+++ NewCPSC481/NewCPSC481/SetGoalPopup.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewCPSC481
+{
+    /// <summary>
+    /// Interaction logic for SetGoalPopup.xaml
+    /// </summary>
+    public partial class SetGoalPopup : Page
+    {
+        public SetGoalPopup()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
