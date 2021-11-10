a5ab5f6229b3e7f8773d8534fa6eba07efff5e2e
bencook0 <bencook03@gmail.com>
2021-11-09 17:34:05 -0700

    DataRepository created

---
+++ NewCPSC481/NewCPSC481/Data/DataRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+namespace NewCPSC481.Data
+{
+    public class DataRepository
+    {
+        private static DataRepository singleton;
+
+        public List<User> Users { get; set; }
+
+        private void Add(User toAdd)
+        {
+            Users ??= new List<User>();
+            Users.Add(toAdd);
+        }
+
+        public static DataRepository Instance => singleton ??= InitializeData();
+
+        private static DataRepository InitializeData()
+        {
+            var repository = new DataRepository();
+            repository.Add(User.Create("User1", "pass1", 10000))
+            return repository;
+        }
+    }
+}
---
+++ NewCPSC481/NewCPSC481/Data/User.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace NewCPSC481.Data
+{
+    public class User
+    {
+        public string DeviceId { get; set; }
+        public string Password { get; set; }
+        
+        public List<int> StepsTaken {get; set; }
+        
+        public User() => StepsTaken = new List<int>();
+
+        public static User Create(string deviceId, string password, int steps)
+        {
+            return new User
+            {
+                DeviceId = deviceId,
+                Password = password,
+                StepsTaken = new List<int> {steps}
+            };
+        }
+    }
+}
