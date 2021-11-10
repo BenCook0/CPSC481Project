44646cd17aacee92108163f6736703dd87a9df64
BenCook0 <bencook03@gmail.com>
2021-11-10 10:09:05 -0700

    created a second user

--- NewCPSC481/NewCPSC481/Data/DataRepository.cs
+++ NewCPSC481/NewCPSC481/Data/DataRepository.cs
@@ -28,22 +28,44 @@ namespace NewCPSC481.Data
 
         private static DataRepository InitializeData()
         {
+            //initalize repository
             var repository = new DataRepository();
 
-            //manually build some users here
-
-
-            Workout a = new Workout(
-                new DateTime(2021, 04, 30), 
+            //build workouts
+            Workout WorkoutA = new Workout(
+                new DateTime(2021, 10, 30), 
                 new List<(string, int, int)> { 
                     ("Pushup", 5, 12),
-                    ("Situp", 3, 9,
+                    ("Situp", 3, 9),
                     ("Bicep Curls", 4, 10)
                 });
 
-            List <Workout> User1Workouts = new List<Workout> { a,a,a };
+            Workout WorkoutB = new Workout(
+                new DateTime(2021, 10, 31),
+                new List<(string, int, int)> {
+                    ("Pushup", 8, 9),
+                });
+
+            Workout WorkoutC = new Workout(
+                new DateTime(2021, 11, 01),
+                new List<(string, int, int)> {
+                    ("Bench Press", 4, 10),
+                    ("Leg Press", 4, 11),
+                    ("Lunges", 6, 10),
+            });
+
+            Workout WorkoutD = new Workout(
+                new DateTime(2021, 11, 05),
+                new List<(string, int, int)> {
+                    ("Situp", 8, 9),
+            });
+
+
+            List <Workout> User1Workouts = new List<Workout> { WorkoutA, WorkoutB, WorkoutC, WorkoutD };
+            List<Workout> User2Workouts = new List<Workout> {WorkoutB, WorkoutD };
 
             repository.Add(User.Create("User1", "Pass1", User1Workouts));
+            repository.Add(User.Create("User2", "Pass2", User2Workouts));
             return repository;
         }
     }
